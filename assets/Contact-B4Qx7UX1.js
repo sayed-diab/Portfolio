c4867572827d7e4d8794c6ba0d5587bc4cfa8fc8
sayed-diab <sayed-diab@users.noreply.github.com>
2025-02-15 22:08:00 +0000

    deploy: 5f8b269c0ec8496c5f6d4405b8cac0fc78427950

---
+++ assets/Contact-B4Qx7UX1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CjFi9FA5.js";const a=()=>t.jsx("section",{className:"contact",id:"contact",children:t.jsx("img",{src:"../assets/favicon.webp",alt:""})});export{a as default};
---
+++ assets/index-BTTx9K4z.js
@@ -0,0 +1,1 @@
+import{c as r,g as t}from"./index-CjFi9FA5.js";var a=r();const e=t(a);export{e as R,a as r};
