094a070046493e9e19e6a1e24dcc5a58ea401b1d
sayed-diab <sayed-diab@users.noreply.github.com>
2025-02-23 10:57:47 +0000

    deploy: 7d4d616ae1591f63b6c9aa029ebc1a7e21b30e2c

---
+++ assets/AsideBar-AihmcqOt.js
@@ -0,0 +1,1 @@
+import{a as s,j as a}from"./index-CF4Ra9tS.js";const i=()=>s("section",{className:"aside-bar",children:a("div",{className:"logo-container",children:[s("img",{src:"./assets/favicon.webp",alt:"logo"}),a("h1",{children:["sayed",s("b",{children:"Diab"})]})]})});export{i as default};
