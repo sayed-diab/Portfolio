dc3058dbf5854c46ecb726ed632220d899460128
sayed-diab <sayeddebo95@gmail.com>
2025-02-23 16:37:25 +0200

    Add image optimization and visualization plugins; update Vite configuration for improved build performance

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,34 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import compression from 'vite-plugin-compression';
-import analyze from 'rollup-plugin-analyzer';
 import Inspect from 'vite-plugin-inspect';
 import dynamicImport from 'vite-plugin-dynamic-import';
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
-  plugins: [dynamicImport(), Inspect(), compression({ algorithm: 'brotliCompress' }), react(), analyze()],
+  plugins: [
+    viteImagemin({
+      gifsicle: { optimizationLevel: 3 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.8, 0.9] },
+      svgo: { plugins: [{ removeViewBox: false }] },
+    }),
+
+    dynamicImport(),
+    Inspect(),
+    compression({ algorithm: 'brotliCompress' }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   base: '/',
+  optimizeDeps: {
+    include: ['lodash-es'], // Pre-bundle specific dependencies
+  },
+  build: {
+    target: 'es2015', // Target modern browsers
+  },
 });
