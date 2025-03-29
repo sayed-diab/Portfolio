e74e0ae3674fbe8786c68ffbee8a9436c2e1772a
sayed-diab <sayeddebo95@gmail.com>
2025-03-29 09:33:10 +0200

    Add Buttonizer script and refactor About component for improved content presentation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,13 +2,6 @@ import '../styles/about.css';
 import { motion } from 'framer-motion';
 
 const About = () => {
-  const textArr = [
-    'Hi 👋, I am Sayed Diab, I am a passionate front-end developer with over 5 years of experience in building modern and responsive web applications since 2019. I specialize in technologies like HTML, CSS, JavaScript, and React, and I am constantly expanding my skills to include frameworks such as Next.js, SASS, and Node.js.',
-    'I have also completed a front-end development course provided by the Egyptian Ministry of Communications and Information Technology in collaboration with Udacity, which enhanced my technical skills and deepened my understanding of industry best practices.',
-    'I have had the privilege of working with numerous clients through the Khamsat platform, successfully delivering high-quality projects that meet their unique needs. My experience on the platform has sharpened my ability to communicate effectively, understand client requirements, and deliver results within deadlines.',
-    'I am currently looking for a remote full-time position where I can contribute my expertise and continue to grow as a developer. My goal is to create user-friendly websites that provide seamless experiences, combining creativity and attention to detail.',
-    'Whether working as a freelancer or as part of a team, I am committed to delivering exceptional results that exceed expectations. Let’s work together to bring your ideas to life!',
-  ];
   return (
     <section
       className="about"
@@ -19,25 +12,18 @@ const About = () => {
       </h1>
       <div className="about-content">
         <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
+          initial={{ opacity: 0, filter: 'blur(10px)' }}
+          whileInView={{ opacity: 1, filter: 'blur(0)' }}
           transition={{ duration: 1 }}
           viewport={{ once: true, amount: 0.7 }}
         >
-          {textArr.map((com, index) => {
-            return (
-              <>
-                <motion.span
-                  initial={{ opacity: 0, x: -10 }}
-                  whileInView={{ opacity: 1, x: 0 }}
-                  transition={{ delay: 0.3 * index, duration: 0.5 }}
-                  viewport={{ once: true, amount: 0.7 }}
-                >
-                  <p>{com}</p>
-                </motion.span>
-              </>
-            );
-          })}
+          <p>
+            Hi 👋, I am Sayed Diab, a passionate front-end developer with <b>over +5 years of experience</b> in building modern and responsive web applications since 2019. I specialize in <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, and <b>React</b>. I'm also expanding my expertise in frameworks like <b>Next.js</b>, <b>SASS</b>, and <b>Node.js</b>.
+          </p>
+          <p>I have also completed a front-end development course provided by the Egyptian Ministry of Communications and Information Technology in collaboration with Udacity, which enhanced my technical skills and deepened my understanding of industry best practices.</p>
+          <p>I have had the privilege of working with numerous clients through the Khamsat platform, successfully delivering high-quality projects that meet their unique needs. My experience on the platform has sharpened my ability to communicate effectively, understand client requirements, and deliver results within deadlines. </p>
+          <p>I am currently looking for a remote full-time position where I can contribute my expertise and continue to grow as a developer. My goal is to create user-friendly websites that provide seamless experiences, combining creativity and attention to detail. </p>
+          <p>Whether working as a freelancer or as part of a team, I am committed to delivering exceptional results that exceed expectations. I'm excited to collaborate on impactful projects and bring ideas to life with clean, efficient, and scalable code. Let's connect!</p>
         </motion.div>
       </div>
     </section>
